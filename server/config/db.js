3514e6be9ff62b002f99401c389ddf46f69cc125
Christian Stirling <77996757+christianstirling@users.noreply.github.com>
2025-06-25 14:36:16 -0500

    Make explicit db path
    Not sure if this is best practice--trying to get Render deployment to connect with MongoDB

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const connectDB = async () => {
 
     try {
         mongoose.set('strictQuery', false)
-        const conn = await mongoose.connect(process.env.MONGODB_URI)
+        const conn = await mongoose.connect('mongodb+srv://cls:db-password-example@cluster0.lglyvc4.mongodb.net/news-app')
         console.log(`* * SERVER>CONFIG>DB: database connected - ${conn.connection.host}`)
     } catch (error) {
         console.log('* * SERVER>CONFIG>DB: error connecting database!')
