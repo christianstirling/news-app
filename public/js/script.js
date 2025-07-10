8c5a684e99d8c5ff57f7ec2c610a073f1c14245a
Christian Stirling <77996757+christianstirling@users.noreply.github.com>
2025-07-10 12:01:31 -0500

    Change button effects

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,57 @@
 
 // Part 1 - Variables
 
+    // creates buttons in JS for each of the filter-buttons--in an array
 const filterButtons = document.querySelectorAll('.filter-button')
+    // container for the grid--will be populated during client-side rendering using fetch
 const container = document.querySelector('.grid')
+
+    // value that currently resides in the header search box
 const searchInput = document.querySelector('.search-input')
+
+    // 'see more' button at the bottom of the page--will be used to generate additional articles
 const seeMoreButton = document.querySelector('#see-more-button')
 
-
-
+    // debounce is used to set a delay from when the user enters/removes any text from
+    // the search bar to when the articles on the page update
+    // this is so that the articles will only render when the user is done typing
 const debouncedResetAndFetchArticles = debounce(resetAndFetchArticles, 300)
 
+    // a Set that holds all of the currently selected categories. 
+    // used in rendering the articles on the page
 const selectedCategories = new Set()
 
+    // empty string that will later hold the string value from searchInput
 let searchText = ''
 
+    // for pagination:
+
+    // current page -- starts at 1
 let currentPage = 1
+    // number of articles that should show up on each 'page'
 const perPage = 12
+    // var to see if there are any more articles to see beyond those shown
 let hasMore = false
+
+
 
 
 
 // Part 2 - Functions
 
+    /* 
+        fetchArticles
+    
+        The main function called when collecting the 'articles' to be rendered on the page
 
+        Works in 2 parts--part 1: creates a URL Search Param used to pull the appropriate articles
+        from the DB, based on the selected categories or the string value in the search
+        box.
+
+        part 2: uses that search param to fetch the resulting JSON data and then calls a function
+        which renders that data onto the page
+
+     */
 function fetchArticles(append = false) {
     const categories = Array.from(selectedCategories).join(',')
     const queryParams = new URLSearchParams()
@@ -40,12 +69,11 @@ function fetchArticles(append = false) {
     fetch(`/articles/?${queryParams.toString()}`)
     .then(res => res.json())
     .then(data => {
-        if(append) {
-            appendArticles(data.articles)
-        } else {
+        if(!append) {
             container.innerHTML = ''
-            renderArticles(data.articles)
         }
+
+        appendArticles(data.articles)
 
         hasMore = data.hasMore
         seeMoreButton.style.display = hasMore ? 'inline-block' : 'none'
@@ -56,15 +84,10 @@ function fetchArticles(append = false) {
     })
 }
 
-function resetAndFetchArticles() {
-    currentPage = 1
-    fetchArticles(false)
-}
-
-function renderArticles(articles) {
-    container.innerHTML = ''
-    appendArticles(articles)
-}
+    /*  
+        Function called in the fetchArticles function--builds the card components and populates
+        them with data from the JSON objects created inside fetchArticles
+    */
 
 function appendArticles(articles) {
 
@@ -144,6 +167,29 @@ function appendArticles(articles) {
     })
 }
 
+    /*  
+        resetAndFetchArticles
+
+        Wrapper function that resets the 'currentPage' index back to the first page,
+        and then runs fetchArticles again.
+
+        Used when user actions are taken that would change the URL search parameters
+        (AKA the categories or the search input is changed)
+    */
+
+function resetAndFetchArticles() {
+    currentPage = 1
+    fetchArticles(false)
+}
+
+    /*
+        debounce
+        
+        Function used to delay the implementation of another function 'func'.
+
+        Used for the search box--so that the page doesn't update until the user is done typing.
+    */
+
 function debounce(func, delay) {
     let timeoutId
     return function (...args) {
@@ -154,14 +200,38 @@ function debounce(func, delay) {
     }
 }
 
+    /*
+        isMobile
+
+        Simple function used to check if the browser is a mobile browser or not.
+
+        Used in the next function to see which button effects to add to the buttons.
+
+        Potential to be used in other ways later as well, as an alternative to media-querying.
+    */
 
 function isMobile() {
     return /Mobi|Android/i.test(navigator.userAgent)
 }
 
+    /*
+        setButtonEffects
+
+        Checks to see if the browser is on Mobile or not, if so:
+
+        Adds the .active-touch class to the button for a given amount of time.
+        Purpose of this is to create a button effect that works on mobile, whereas
+        the previous effects work by using the hover state, which is not used
+        in mobile.
+
+        In the CSS, the active-touch class adds similar effects to the buttons as
+        what you would see on desktop using hover.
+    */
+
 function setButtonEffects() {
     if(isMobile()) {
         const buttons = document.querySelectorAll('.button')
+        const time = 2000
 
         buttons.forEach(button => {
             button.addEventListener('touchstart', () => {
@@ -169,13 +239,19 @@ function setButtonEffects() {
 
                 setTimeout(() => {
                     button.classList.remove('active-touch')
-                }, 2000)
+                }, time)
             })
         })
     }
 }
 
+
+
 // Part 3 - Event Listeners
+
+    /*
+        
+    */
 
 filterButtons.forEach(button => button.addEventListener('click', () => {
 
@@ -258,12 +334,20 @@ container.addEventListener('click', (event) => {
     }
 })
 
+
+
+
+
+
+
 seeMoreButton.addEventListener('click', () => {
     currentPage++
     fetchArticles(true)
 })
 
 // Part 4 - Main
+
 setButtonEffects()
+
 fetchArticles()
 
